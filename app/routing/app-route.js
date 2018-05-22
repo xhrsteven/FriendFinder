dd0ba1a4f1125bd2f2d7d4d12a4d7a915e6a4649
Steven <xhrsteven@gmail.com>
2018-05-22 17:48:43 -0500

    updddddd

--- app/routing/app-route.js
+++ app/routing/app-route.js
@@ -6,8 +6,31 @@ module.exports = function (app) {
       });
 
     app.post('/api/friends', function (req, res) {
-        friendData.push(req.body);
-        res.json(true);
-      })
+        // friendData.push(req.body);
+        // res.json(true);
+      var newFriendScores = req.body.scores;
+      var scoresArray = [];
+      var friendCount = 0;
+      var bestMatch = 0;
+
+      for(var i =0; i<friendData.length; i++){
+        var scoreDiff = 0;
+        for(var j=0; j<newFriendScores.length;j++){
+          scoreDiff += (Math.abs(parseInt(friendData[i].scores[j]) - parseInt(newFriendScores[j])))
+        }
+        scoresArray.push(scoreDiff);
+      }
+
+      for(var i =0; i<scoresArray.length; i++){
+        if(scoresArray[i]<=scoresArray[bestMatch]){
+          bestMatch = i;
+        }
+      }
+
+      var bestFriend = friendData[bestMatch];
+      res.json(bestFriend);
+
+      friendData.push(req.body);
+      });
   }
 
